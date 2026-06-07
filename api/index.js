027954c4b4557da48b250477e92497d9cb19d24f
JCPrime01 <vanzokiller@gmail.com>
2026-06-06 22:17:14 -0300

    chore: shareCode NSQACEYBC0T -> TO8QEC0V9VT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=NSQACEYBC0T&afp=ti' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=TO8QEC0V9VT&afp=ti' });
     res.end();
   }
